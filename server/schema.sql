51cd19a3cc9517f65431c2095faea2f0a2f1486c
liezelanne9 <liezelanne9@gmail.com>
2019-03-13 15:52:51 -0700

    Created tables in chat database

--- server/schema.sql
+++ server/schema.sql
@@ -4,14 +4,22 @@ USE chat;
 
 CREATE TABLE messages (
   /* Describe your table here.*/
+  id INTEGER PRIMARY KEY,
+  message TEXT NOT NULL,
+  room TEXT NOT NULL,
+  user_id INTEGER
 );
 
 /* Create other tables and define schemas for them here! */
-
+CREATE TABLE users (
+  /* Describe your table here.*/
+  id INTEGER PRIMARY KEY,
+  username TEXT NOT NULL
+);
 
 
 
 /*  Execute this file from the command line by typing:
- *    mysql -u root < server/schema.sql
+ *    mysql -u student < server/schema.sql
  *  to create the database and the tables.*/
 
