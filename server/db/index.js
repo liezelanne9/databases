ed404b164a012664c4687df7c2fa6bc38a1a8c74
liezelanne9 <liezelanne9@gmail.com>
2019-03-13 17:05:44 -0700

    Connected database to localhost

--- server/db/index.js
+++ server/db/index.js
@@ -1,7 +1,32 @@
 var mysql = require('mysql');
 
 // Create a database connection and export it from this file.
-// You will need to connect with the user "root", no password,
+// You will need to connect with the user "student", no password,
 // and to the database "chat".
 
 
+const con = mysql.createConnection({
+  host: `localhost`, 
+  port: 3306,
+  user: 'student',
+  password: 'student'
+});
+
+con.connect((err) => {
+  if(err){
+    console.log('Error connecting to Db', err);
+    return;
+  }
+  console.log('Connection established');
+});
+
+
+
+
+
+
+
+
+
+
+//port: 3306
